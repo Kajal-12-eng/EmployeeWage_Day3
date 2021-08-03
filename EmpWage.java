0fddf6ad4df489630441f1f0b92a77425685e8a6
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-03 07:43:38 +0530

    Welcome to Employee Wage Program

---
+++ EmpWage.java
@@ -0,0 +1,18 @@
+package com.employeewage;
+
+public class EmpWage {
+	public static void main(String[] args) {
+			System.out.println("Welcome to the employee wage program");
+			
+			 int IS_FULL_TIME = 1;
+			 double empCheck = Math.floor(Math.random()*10)%2;
+		      if(empCheck == IS_FULL_TIME)
+		    	  System.out.println("Employee is present ");
+		      else
+		    	  System.out.println("Employee is absent ");
+			 
+		      
+		      
+		      
+	}
+}
