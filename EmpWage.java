6aa627ecbb6e1439483b8c7f3f57e256c4d1ce45
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-03 17:04:30 +0530

    Iterating the loop till working hrs or days reached a month

--- EmpWage.java
+++ EmpWage.java
@@ -1,27 +1,27 @@
 package com.employeewage;
 
-public class EmpWage {
+ public class EmpWage {
 	public static void main(String[] args) {
 			System.out.println("Welcome to the employee wage program");  
 			
 			    int IS_FULL_TIME = 1;
-
 			    int IS_PART_TIME = 2;
 			    int EMP_RATE_PER_HOUR = 20;
 			    int NUM_OF_WORKING_DAYS = 20;
-			    int empWage = 0;
-			    int workingHour = 0, totalEmpWage = 0;
-			    for (int day = 0 ;day < NUM_OF_WORKING_DAYS ;day++) {
-		       	    int IS_PART_TIME = 2;
-			    int dailyWage;
-			    int wagePerHour = 20;
-			    int workingHour;
+			    int MAX_HRS_IN_MONTH = 100;
+			    int empWage = 0,  totalEmpWage= 0 ;
+			    int workingHour = 0,  totalWorkingHours= 0;
+			    int totalWorkingDays = 0;
+			    
+			    while (totalWorkingHours <=  MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			    	totalWorkingDays++;
 				int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
 				switch (empCheck) {
 				case 1:
-					System.out.println("Employee is full time present");
+				System.out.println("Employee is full time present");
 					workingHour = 8; 
 					break;
+					
 				case 2:
 					System.out.println("Employee is part time present");
 					workingHour = 4; 
@@ -30,10 +30,11 @@ public class EmpWage {
 					System.out.println("Employee is absent");
 					workingHour = 0;
 				}
-				empWage = workingHour * EMP_RATE_PER_HOUR;
-				totalEmpWage += empWage;
-				System.out.println("Employee  wage is " + empWage);
+				totalWorkingHours += totalWorkingHours;
+				System.out.println("Day:" +totalWorkingDays+ "Working hour is :" + workingHour);
+			
 			    }
-			    System.out.println("Total Employee  wage is " + totalEmpWage);    
+				totalEmpWage = totalWorkingHours * EMP_RATE_PER_HOUR;
+			    System.out.println("Total Employee  wage is: " + totalEmpWage);    
 	}
 }
