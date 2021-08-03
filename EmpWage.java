067908c55a3d8a7bc5fc4c34cdbcf117f6774783
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-03 07:58:16 +0530

    Employee daily wage

--- EmpWage.java
+++ EmpWage.java
@@ -2,17 +2,27 @@ package com.employeewage;
 
 public class EmpWage {
 	public static void main(String[] args) {
-			System.out.println("Welcome to the employee wage program");
+			System.out.println("Welcome to the employee wage program");  
 			
-			 int IS_FULL_TIME = 1;
-			 double empCheck = Math.floor(Math.random()*10)%2;
-		      if(empCheck == IS_FULL_TIME)
-		    	  System.out.println("Employee is present ");
-		      else
-		    	  System.out.println("Employee is absent ");
-			 
-		      
-		      
-		      
+			   
+		     int IS_FULL_TIME = 1;
+				int dailyWage;
+				int wagePerHour = 20;
+				int workingHour;
+				int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
+				if( empCheck == IS_FULL_TIME)
+				{
+					System.out.println("Employee is present");
+					workingHour = 8; 
+				}
+				else
+				{
+					System.out.println("Employee is absent");
+					workingHour = 0;
+				}
+				
+				dailyWage = workingHour * wagePerHour;
+				System.out.println("Employee daily wage is " + dailyWage);
+
 	}
 }
