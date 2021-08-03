bd6b117ecbe6834bbeb6d541f52179b83155e05a
kajal-12-eng <kajalsingh85654@gamil.com>
2021-08-03 10:27:19 +0530

    Employee part time

--- EmpWage.java
+++ EmpWage.java
@@ -4,16 +4,21 @@ public class EmpWage {
 	public static void main(String[] args) {
 			System.out.println("Welcome to the employee wage program");  
 			
-			   
-		     int IS_FULL_TIME = 1;
-				int dailyWage;
-				int wagePerHour = 20;
-				int workingHour;
-				int empCheck = (int)(Math.floor(Math.random() * 10)) % 2;
+			    int IS_FULL_TIME = 1;
+		        int IS_PART_TIME = 2;
+			    int dailyWage;
+			    int wagePerHour = 20;
+			    int workingHour;
+				int empCheck = (int)(Math.floor(Math.random() * 10)) % 3;
 				if( empCheck == IS_FULL_TIME)
 				{
-					System.out.println("Employee is present");
+					System.out.println("Employee is full time present");
 					workingHour = 8; 
+				}
+				else if( empCheck == IS_PART_TIME)
+				{
+					System.out.println("Employee is part time present");
+					workingHour = 4; 
 				}
 				else
 				{
@@ -23,6 +28,5 @@ public class EmpWage {
 				
 				dailyWage = workingHour * wagePerHour;
 				System.out.println("Employee daily wage is " + dailyWage);
-
 	}
 }
